95713530afebe405faecd2525c6a89ad5e35d926
nasirdiu <nasir.joypurhat@gmail.com>
2023-06-26 21:26:20 +0600

    skill submit complete

--- resources/views/components/professional-skills.blade.php
+++ resources/views/components/professional-skills.blade.php
@@ -5,19 +5,43 @@
                 <h2 class="text-secondary fw-bolder mb-4">Professional Skills</h2>
                 <div class="card shadow border-0 rounded-4 mb-5">
                     <div class="card-body p-5">
-                        <div class="row row-cols-1 row-cols-md-3 mb-4">
-                            <div class="col mb-4 mb-md-0"><div class="d-flex align-items-center bg-light rounded-4 p-3 h-100">SEO/SEM Marketing</div></div>
-                            <div class="col mb-4 mb-md-0"><div class="d-flex align-items-center bg-light rounded-4 p-3 h-100">Statistical Analysis</div></div>
-                            <div class="col"><div class="d-flex align-items-center bg-light rounded-4 p-3 h-100">Web Development</div></div>
+                        <div id="skill-list" class="row row-cols-1 row-cols-md-3 mb-4">
+
                         </div>
-                        <div class="row row-cols-1 row-cols-md-3">
-                            <div class="col mb-4 mb-md-0"><div class="d-flex align-items-center bg-light rounded-4 p-3 h-100">Network Security</div></div>
-                            <div class="col mb-4 mb-md-0"><div class="d-flex align-items-center bg-light rounded-4 p-3 h-100">Adobe Software Suite</div></div>
-                            <div class="col"><div class="d-flex align-items-center bg-light rounded-4 p-3 h-100">User Interface Design</div></div>
-                        </div>
+
                     </div>
                 </div>
             </section>
         </div>
     </div>
 </div>
+
+<script>
+    SkillList();
+    async function SkillList(){
+        try{
+            let URL='/skillsData'
+            let response=await axios.get(URL);
+
+            debugger;
+            document.getElementById('loading-div').classList.add('d-none');
+            document.getElementById('content-div').classList.remove('d-none');
+
+            response.data.forEach((item)=>{
+                document.getElementById('skill-list').innerHTML +=(
+                    `
+               <div class="col p-2 mb-4 mb-md-0">
+                    <div class="d-flex align-items-center bg-light rounded-4 p-3 h-100">
+                       ${item['name']}
+                    </div>
+                    </div>
+                `
+                )
+
+            })
+        }
+        catch (error) {
+            alert(error);
+        }
+    }
+</script>
